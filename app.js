4b7b1df585baf3cf106ad58cb342d0db60f78aad
seif chalabi <seif12244@gmail.com>
2022-02-17 21:56:54 +0300

    added a feature that when you click the button it adds a hello world to the website

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-console.log("hello world")
-function hello(){
-  console.log("hello world")
-}
-hello()
+let btn = document.querySelector("button")
+let ul = document.querySelector("ul")
+let body = document.body
+btn.addEventListener("click",()=>{
+    let li = document.createElement("li")
+    li.textContent = "Hello world"
+    ul.append(li)
+    li.addEventListener("dblclick",()=>{
+        li.remove()
+    })
+})
